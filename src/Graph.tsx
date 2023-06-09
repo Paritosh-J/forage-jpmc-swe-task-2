7d1de45c5d92754eb29ab2644c69f4ce4ef207cd
Paritosh-J <paritoshj2001@gmail.com>
2023-06-09 18:33:41 +0530

    Graph.tsx updation

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -36,20 +36,6 @@ class Graph extends Component<IProps, {}> {
       "perspective-viewer"
     )[0] as unknown as PerspectiveViewerElement;
 
-    elem.setAttribute("view", "y_line");
-    elem.setAttribute("column-pivots", '["stocks"]');
-    elem.setAttribute("row-pivots", '["timestamp"]');
-    elem.setAttribute("columns", '["top_ask_price"]');
-    elem.setAttribute(
-      "aggregates",
-      `
-      {"stock":"distant count",
-      "top_ask_price":"avg",
-      "top_bid_price":"avg",
-      "timestamp":"distinct count"}
-    `
-    );
-
     const schema = {
       stock: "string",
       top_ask_price: "float",
@@ -65,6 +51,19 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row-pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute(
+        "aggregates",
+        JSON.stringify({
+          stock: "distinctcount",
+          top_ask_price: "avg",
+          top_bid_price: "avg",
+          timestamp: "distinct count",
+        })
+      );
     }
   }
 
